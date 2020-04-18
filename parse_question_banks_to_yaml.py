130c3a95e60a92f5f94259cbe13a088a9ee96d6d
Denis Oakley <20573568+Denis-Oakley@users.noreply.github.com>
2020-04-18 15:33:09 +0800

    support new question bank format

--- parse_question_banks_to_yaml.py
+++ parse_question_banks_to_yaml.py
@@ -1,55 +1,67 @@
 # -*- coding: UTF-8 -*-
-import yaml
-import os
 import re
+from pathlib import Path
+
+import yaml
 
 
-def parseQuestionsFromTxt(questions):
-    questions = re.sub('([ABCDEFG])\t', r'\1、', questions)
-    questions = onlyKeepChineseWords(questions)
-    questions = re.split(r'\n+?[0-9]+、', questions)  # 分割题目
-    del questions[0]
-    # 保证所有选项独立成行
-    # answers = re.sub(r'([^\na-zA-Z0-9])([ABCDEFG]、)', r'\1\n\2', answers)
-    _questions = {}
-    for question in questions:
-        question = question.replace('\n', '')  # 题目+选项+答案
-        problem = None  # 题目
-        options = []  # 选项
+def parseQuestionsFromTxt(questionBanksI):
+    questionBanksI = questionBanksI.replace('（', '(')
+    questionBanksI = questionBanksI.replace('）', ')')
+    questionBanksI = re.sub(r'((?<!^[(\[]) )|( (?![)\]]))', '', questionBanksI, flags=re.M)
+    questionBanksI = re.sub(r'^\t+', '', questionBanksI, flags=re.M)
+    questionBanksI = re.sub(r'[^\u4e00-\u9fa5、. ()\[\]a-zA-Z0-9\t\n]', '', questionBanksI)
+    questionBanksI = re.sub(r'(?<=[^\n]\n)([ABCDEFG])[\t、.]+', r'\n`\1`', questionBanksI, flags=re.M)
+    questionBanksI = re.sub(r'(?<=[^\n]\n)[(\[]([Xx ])[)\]]', r'`\1`', questionBanksI, flags=re.M)
+    questionBanksI = re.sub(r'(参考)?答案?(?P<answer>[ABCDEFG对错]+)$', r'`!\g<answer>`!', questionBanksI, flags=re.M)
+    questionBanksI = re.sub(r'\((?P<answer>[ABCDEFG对错]+)\)$', r'`!\g<answer>`!', questionBanksI, flags=re.M)
+    questionBanksI = re.sub(r'[\[\]()\t、]', '', questionBanksI)
+    while '\n\n\n' in questionBanksI:
+        questionBanksI = questionBanksI.replace('\n\n\n', '\n\n')
+    questionBanksI = re.sub(r'\n+$', '', questionBanksI)
+    questionsBanksDict = {}
+
+    for questionBank in questionBanksI.split('\n\n'):
+        question = None  # 题目
+        options = None  # 选项
         answers = None  # 答案
-        if re.search(r'A[、.].+?B[、.]', question) is None:
-            # 判断题
-            parts = re.search(r'(.+?)([对错]$)', question)
-            _questions[parts.group(1)] = ['0' if parts.group(2) == '对' else '1']
+        lines = questionBank.split('\n')
+        searchQuestion = re.search(r'(.+)`!(.+)`!', lines[0])
+        searchAnswer = re.search(r'`!(.+)`!', lines[-1])
+        if searchQuestion is not None:
+            question = searchQuestion.group(1)
+            answers = searchQuestion.group(2)
         else:
-            # 选择题
-            # ABCDEFG
-            for i in range(6):
-                parts = re.split('{}[、.]'.format(chr(65 + i)), question)
-                if len(parts) == 1 and i < 4:  # ABCD
-                    parts = question.split('{}\t'.format(chr(65 + i)))
-                if len(parts) == 1:  # 循环到不存在的选项
-                    lastParts = re.search('(.+?)(参考)?答案(A?B?C?D?E?F?G?)', question)
-                    if lastParts is None:
-                        options.append(parts[0])
-                    else:
-                        # 答案在尾部
-                        answers = lastParts.group(3)
-                        options.append(lastParts.group(1))
-                    break
-                question = parts[1]
-                if i == 0:
-                    problem = parts[0]
-                    continue
-                options.append(parts[0])
-            if answers is None:
-                problemParts = re.search(r'(.+?)(A?B?C?D?E?F?G?)$', problem)
-                problem = problemParts.group(1)
-                answers = problemParts.group(2)
-            options.sort()  # 保证统一
-            # {'题目_选项': ['0', '1', '2']}                      # ord('A') - ord('0') = 17
-            _questions['_'.join((problem, *options))] = [chr(ord(i) - 17) for i in answers]
-    return _questions
+            question = lines[0]
+            if searchAnswer is not None:
+                answers = searchAnswer.group(1)
+        if searchAnswer is not None:
+            options = lines[1:-1]
+        else:
+            options = lines[1:]
+        if answers is None:
+            answers = []
+            for option, line in enumerate(options):
+                searchOption = re.search(r'^`(.)`', line)
+                if searchOption is not None:
+                    if searchOption.group(1) != ' ':
+                        answers.append(str(option))  # 0, 1, 2
+        if len(options) == 2:
+            options = []
+        for index, option in enumerate(options):
+            options[index] = re.sub(r'^`.`', '', option)
+        if answers == '对':
+            answers = ['0']
+        elif answers == '错':
+            answers = ['1']
+        elif re.search(r'[0-9]', answers[0]) is None:
+            # ord('A') - ord('0') = 17
+            answers = [chr(ord(i) - 17) for i in answers]
+        options.sort()  # 保证统一
+        # {'题目_选项': ['0', '1', '2']}
+        questionsBanksDict['_'.join((question, *options))] = answers
+
+    return questionsBanksDict
 
 
 def parseQuestionsFromHtml(questions):
@@ -57,7 +69,7 @@ def parseQuestionsFromHtml(questions):
                          r'[0-9]+</span><span[\n\s]+?class="cls_002">、', questions)
     del questions[0]
     del questions[-1]
-    _questions = {}
+    questions = {}
     for question in questions:
         # 答案标签 class
         answers = re.findall(r'<span[\n\s]+class="cls_005">(.+?)</span>'
@@ -65,35 +77,28 @@ def parseQuestionsFromHtml(questions):
         # 合并
         answers = [answer[0] + answer[2] for answer in answers]
         # 移除空白填空
-        # answers = list(filter(lambda item: not item.isspace(), answers))
         question = re.sub(r'<span[\n\s]+class="cls_00[57]">.+?</span>', '', question)
         question = re.sub(r'<.+?>', '', question, flags=re.S)
         # 只保留汉字,数字,字母等字符, 适用于填空题
         question = re.sub(r'[^\u4e00-\u9fa5、.a-zA-Z0-9]', '', question)
-        _questions[question] = answers
-        # questions = []
-        # soup = BeautifulSoup(file, "lxml")
-        # for i in soup.find_all(class_='cls_003'):
-        #     i.getText()
-        # for tag in soup.find_all(getBlankTag):
-        #     tag.string.replace_with('___')
-    return _questions
-
-
-def onlyKeepChineseWords(string):
-    # 只保留汉字,数字,字母,换行等字符, 不适用于填空题
-    return re.sub(r'[^\u4e00-\u9fa5、.a-zA-Z0-9\n]', '', string)
+        questions[question] = answers
+    return questions
 
 
 questionBanks = {}
-
-with os.scandir('Question-Banks') as files:
-    for entry in files:
-        with open(entry, 'r', encoding='UTF-8') as file:
-            if file.name.endswith('.html'):
-                questionBanks = {**questionBanks, **parseQuestionsFromHtml(file.read())}
-            else:
-                questionBanks = {**questionBanks, **parseQuestionsFromTxt(file.read())}
-
-with open('Question-Banks.yaml', 'w', encoding='UTF-8') as file:
-    yaml.safe_dump(questionBanks, file, indent=4, allow_unicode=True)
+for file in Path('Question-Banks').iterdir():
+    with file.open('r', encoding='UTF-8') as stream:
+        if file.suffix == '.html':
+            questionBanks = {**questionBanks, **parseQuestionsFromHtml(stream.read())}
+        else:
+            questionBanks = {**questionBanks, **parseQuestionsFromTxt(stream.read())}
+path = Path('Question-Banks.yaml')
+if not path.exists():
+    path.touch()
+with path.open('r+', encoding='UTF-8') as file:
+    questionBanksO = yaml.safe_load(file)
+    if questionBanksO is None:
+        questionBanksO = {'collected': {}}
+    questionBanksO['parsed'] = questionBanks
+    file.seek(0)
+    yaml.safe_dump(questionBanksO, file, indent=4, allow_unicode=True)
